68d15fd7f5efb12adda9437ad6cb4b3d19176020
Leonir Zimmermann <52092300+leonirz@users.noreply.github.com>
2020-08-25 22:52:20 -0300

    Add files via upload

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,11 +1,50 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
+import {startOfHour, parseISO, isEqual } from 'date-fns';
+ 
 const appointmentsRouter = Router();
 
+
+// tipar - interface
+interface Appointment {
+    id: string;
+    provider : "string";
+    date : Date;
+}
+
+
+const appointments: Appointment[] = [];
 
 // quando chamado http://localhost:3333/apointments
 // este é um MIDLEWARE
 appointmentsRouter.post('/', (request, response) => {
-    return response.json({ message: 'Hello World do appointments.route.ts'});
+
+    const { provider, date } = request.body;
+
+    const parsedDate = startOfHour(parseISO(date));
+
+    //garantir que 2 agendamentos não são feitos no mesmo horário
+    const findAppointmentInSameDate = appointments.find(appointment =>
+        isEqual(parsedDate, appointment.date),
+        );
+
+    if(findAppointmentInSameDate){
+        return response
+        .status(400)
+        .json({message: 'This appointment is already booked!'});
+    }
+
+
+    //estrutura appointment
+    const appointment = {
+        id : uuid(),
+        provider,
+        date: parsedDate,
+    };
+
+    appointments.push(appointment);
+
+    return response.json(appointment);
 });
 
 export default appointmentsRouter;     
